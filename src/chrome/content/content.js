22f0b3a6afcde46e02f211c4275a2de93af82734
jwwang <jwwang@mozilla.com>
2017-01-18 14:40:07 +0800

    Bug 1331855. Part 3 - replace string concatenation with DOM APIs for easier data manipulation.

--- src/chrome/content/content.js
+++ src/chrome/content/content.js
@@ -13,20 +13,36 @@ function EscapeHTML(string) {
   });
 }
 
+function addList(parent) {
+  let list = content.document.createElement("ul");
+  if (parent) {
+    parent.appendChild(list);
+  }
+  return list;
+}
+
+function addItem(list, string) {
+  let i = content.document.createElement("li");
+  i.innerText = string;
+  list.appendChild(i);
+  return i;
+}
+
 function MediaDetails(media) {
-  var text = EscapeHTML(content.document.location) + "\n";
+  let top = addList(null);
+  let list = addList(addItem(top, content.document.location));
 
   for (var j=0; j < media.length; ++j) {
-    text += "\t" + EscapeHTML(media[j].currentSrc) + "\n";
-    text += "\t" + "currentTime: " + media[j].currentTime + " readyState: " + media[j].readyState;
+    addItem(list, media[j].currentSrc);
+    addItem(list, "currentTime: " + media[j].currentTime + " readyState: " + media[j].readyState);
     if (media[j].error) {
-      text += " error: " + media[j].error.code;
+      let s = " error: " + media[j].error.code;
       if ((typeof media[j].error.message === 'string' || media[j].error.message instanceof String)
           && media[j].error.message.length > 0) {
-        text += " (" + media[j].error.message + ")";
+        s += " (" + media[j].error.message + ")";
       }
+      addItem(list, s);
     }
-    text += "\n";
 
     let quality = media[j].getVideoPlaybackQuality();
     let ratio = "--"
@@ -34,42 +50,40 @@ function MediaDetails(media) {
       ratio = 100 - Math.round(100 * quality.droppedVideoFrames / quality.totalVideoFrames);
       ratio += "%";
     }
-    text += "\tQuality: " + ratio;
-    text += " (total:" + quality.totalVideoFrames;
-    text += " dropped:" + quality.droppedVideoFrames;
-    text += " corrupted:" + quality.corruptedVideoFrames + ")\n";
+    addItem(list, "Quality: " + ratio +
+                  " (total:" + quality.totalVideoFrames +
+                  " dropped:" + quality.droppedVideoFrames +
+                  " corrupted:" + quality.corruptedVideoFrames + ")");
 
-    text += "\tBuffered ranges: [";
+    let s = "Buffered ranges: [";
     for (var l=0; l < media[j].buffered.length; ++l) {
-      text += "(" + media[j].buffered.start(l) + ", " + media[j].buffered.end(l) + ")";
+      s += "(" + media[j].buffered.start(l) + ", " + media[j].buffered.end(l) + ")";
     }
-    text += "]\n";
+    s += "]";
+    addItem(list, s);
 
     var ms = media[j].mozMediaSourceObject;
     if (ms) {
       for (var k=0; k < ms.sourceBuffers.length; ++k) {
         var sb = ms.sourceBuffers[k];
-        text += "\t\tSourceBuffer " + k + "\n";
+        let s = `SourceBuffer[${k}]: `;
         for (var l=0; l < sb.buffered.length; ++l) {
-          text += "\t\t\tstart=" + sb.buffered.start(l) + " end=" + sb.buffered.end(l) + "\n";
+          s += `(${sb.buffered.start(l)}, ${sb.buffered.end(l)})`;
         }
+        addItem(list, s);
       }
     }
 
     let debugData = media[j].mozDebugReaderData;
     if (debugData) {
-      text += "\tInternal Data:\n";
+      let subList = addList(addItem(list, "Internal Data:"));
       for(let x of debugData.split("\n")) {
-        text += "\t" + x + "\n";
+        addItem(subList, x);
       }
-    }
-
-    if (j < media.length - 1) {
-      text += "\n";
     }
   }
 
-  sendAsyncMessage("aboutmedia-videodetails", { details: text });
+  sendAsyncMessage("aboutmedia-videodetails", { details: top.outerHTML });
 }
 
 var media = content.document.getElementsByTagName("video");
